fe6ce40252c2462cca0f2b72efab196b5d585405
Andreas Sjoberg <ansj@bluegarden.com>
2017-05-12 15:18:15 +0200

    yay

--- web/Scripts/src/Components/topicr.jsx
+++ web/Scripts/src/Components/topicr.jsx
@@ -1,5 +1,6 @@
 ﻿import React from 'react';
 import { render } from 'react-dom';
+import PropTypes from 'prop-types';
 
 class TopicList extends React.Component {
     render() {
@@ -30,16 +31,58 @@ class Topic extends React.Component {
 }
 
 class TopicForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '', description: ''
+        };
+    }
+
+    handleTitleChange(event) {
+        this.setState({ title: event.target.value });
+    }
+
+    handleDescriptionChange(event) {
+        this.setState({ description: event.target.value });
+    }
+
     render() {
         return (
-            <div className="topic-form">
-                My form
-            </div>
+            <form className="form-group">
+                <h2>Post a new topic</h2>
+                <input className="form-control" type="text" placeholder="title" value={this.state.title} onChange={this.handleTitleChange.bind(this)} />
+                <input className="form-control" type="text" placeholder="description" value={this.state.description} onChange={this.handleDescriptionChange.bind(this)} />
+                <button className="btn btn-lg btn-primary" type="submit">Post topic</button>
+            </form>
         );
     }
 }
 
 class Topicr extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+
+    loadTopicsFromServer() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', this.props.url, true);
+        xhr.onload = function () {
+            var data = JSON.parse(xhr.responseText);
+            this.setState(prevState => (
+                { data: data }
+            ));
+        }.bind(this);
+        xhr.send();
+    }
+
+    componentWillMount() {
+        this.loadTopicsFromServer();
+        //window.setInterval(this.loadTopicsFromServer, this.props.pollInterval);
+    }
+
     render() {
         return (
             <div className="topicr">
@@ -48,19 +91,20 @@ class Topicr extends React.Component {
                         <h1>Topicr</h1>
                     </div>
                 </div>
-                <TopicList data={this.props.data} />
+                <TopicList data={this.state.data} />
                 <TopicForm />
             </div>
         );
     }
 }
 
-var data = [
-    { id: 1, title: "Title 1", description: "What's wrong with you?" },
-    { id: 2, title: "Title 2", description: "Oh, come on now" }
-];
+Topicr.propTypes = {
+    id: PropTypes.number,
+    title: PropTypes.string,
+    description: PropTypes.string
+}
 
 ReactDOM.render(
-    <Topicr data={data}/>,
+    <Topicr url="/api/topics" pollInterval={2000}/>,
     document.getElementById('content')
 );
