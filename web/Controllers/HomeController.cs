5fe88de441af55bd77aa2bf9eed03600dfc672e1
Andreas Sjoberg <ansj@bluegarden.com>
2017-05-24 10:58:52 +0200

    Link from URL

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using topicr.Models;
 
 namespace topicr.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly PollContext _db;
+
+        public HomeController(PollContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index(string link = null)
         {
+            if (!string.IsNullOrWhiteSpace(link) && _db.Polls.Any(p => p.Link.Equals(link)))
+            {
+                ViewBag.Link = link;
+            }
             return View();
         }
     }
